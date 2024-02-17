ee8a3fb5c87e95bd73d9e30c702c0fc368f6b478
mahdipratama <mahdipratama29@gmail.com>
2024-02-17 10:30:27 +0700

    add Data Types lecture

--- 01-variables-data-types/03-data-types/index.php
+++ 01-variables-data-types/03-data-types/index.php
@@ -3,12 +3,54 @@
 /*
   PHP DATA TYPES:
 
-- String
-- Integer
-- Float
-- Boolean
-- Array
-- Object
-- NULL
+- String: Sequence of characters, Enclosed in quotes
+- Integer: Whole number. It can be positive or negative 
+- Float: Number with decimal point
+- Boolean: True or False
+- Array: Holds multiple values
+- Object: Collection of properties & methods
+- NULL: Represent a variable with no value
 - Resource
 */
+
+// String
+$name = 'Wilson Peterson';
+$name2 = "Wilson Peterson";
+
+var_dump($name);
+echo '<br/>';
+echo getType($name2);
+
+echo '<br/>';
+
+// Integer 
+$age = 35;
+var_dump($age);
+echo '<br/>';
+
+// Float 
+$rating = 4.5;
+var_dump($rating);
+echo '<br/>';
+
+// Boolean 
+$isLoaded = true;
+var_dump($isLoaded);
+echo '<br/>';
+
+// Array
+$friends = ['John', 'Jax', 'Bob'];
+var_dump($friends);
+echo '<br/>';
+
+// Object 
+$person = new stdClass();
+var_dump($person);
+
+// Null 
+$car = null;
+var_dump($car);
+
+// Resource 
+// $file = fopen('/03-data-types/sample.txt', 'r');
+// var_dump($file);
