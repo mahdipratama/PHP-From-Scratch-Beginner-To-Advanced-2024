0a5cc03504c0df3a7358e12fea359a4aeabbcee0
mahdipratama <mahdipratama29@gmail.com>
2024-02-17 21:22:40 +0700

    add Array Challenge

--- 02-arrays-and-iteration/05-array-challenges/index.php
+++ 02-arrays-and-iteration/05-array-challenges/index.php
@@ -1,4 +1,5 @@
 <?php
+$output = null;
 
 /*
   Challenge 1: Sum of an array
@@ -9,6 +10,22 @@
   5. Print out 'The sum of the {amount} numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be 'The sum of the 5 numbers is: 15'. 
 */
 echo '<h3>Sum Of An Array</h3>';
+$numbers = [1, 2, 3, 4, 5];
+$sum = array_sum($numbers);
+$amount = count($numbers);
+// var_dump($sum, $amount);
+
+function printString($sum, $amount)
+{
+  print_r("The sum of the $amount numbers is: $sum");
+};
+
+printString($sum, $amount);
+
+echo '<br/>';
+echo '<br/>';
+
+
 
 /*
   Challenge 2: Colors array
@@ -24,6 +41,19 @@ You should end up with the following array: ['yellow', 'pink', 'blue', 'red', 'p
 echo '<h3>Colors Array</h3>';
 
 $colors = ['red', 'blue', 'green', 'yellow'];
+$reversedColors = array_reverse($colors);
+array_push($reversedColors, 'purple', 'orange');
+
+// Using array_merge 
+$reversedColors = array_merge($colors, ['purple', 'orange']);
+
+array_splice($reversedColors, 1, 1, 'pink');
+
+print_r($reversedColors);
+
+echo '<br/>';
+echo '<br/>';
+
 
 /*
   Challenge 3: Job listings array
@@ -35,3 +65,77 @@ $colors = ['red', 'blue', 'green', 'yellow'];
 */
 
 echo '<h3>Job Listings</h3>';
+$listings = [
+  [
+    'id' => '1',
+    'job_title' => 'Web Dev',
+    'company' => 'whatever.inc',
+    'contact_email' => 'whatever.inc@gmail.com',
+    'contact_phone' => '233-9982',
+    'skills' => ['PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS']
+  ],
+  [
+    'id' => '2',
+    'job_title' => 'Frontend Dev',
+    'company' => 'fabulous.corp',
+    'contact_email' => 'fabulous.corp@gmail.com',
+    'contact_phone' => '233-9982',
+    'skills' => ['Next.js', 'React.js', 'JavaScript', 'HTML', 'CSS']
+  ],
+  [
+    'id' => '3',
+    'job_title' => 'Backend Dev',
+    'company' => 'whatever.inc',
+    'contact_email' => 'whatever.inc@gmail.com',
+    'contact_phone' => '233-9982',
+    'skills' => ['PHP', 'MySQL', 'JavaScript', 'MongoDB', 'Django']
+  ],
+];
+
+$newList =  [
+  'id' => '4',
+  'job_title' => 'Fullstack Dev',
+  'company' => 'whatever.inc',
+  'contact_email' => 'whatever.inc@gmail.com',
+  'contact_phone' => '233-9982',
+  'skills' => ['PHP', 'MySQL', 'JavaScript', 'MongoDB', 'Django', 'React.js', 'TypeScript', 'Next.js']
+];
+
+array_push($listings, $newList);
+
+$output = $listings[1]['job_title']; // Frontend Dev 
+
+$output = $listings[2]['skills'][0]; // PHP
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+    </div>
+  </header>
+  <div class="container mx-auto p-4 mt-4">
+    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <!-- Output -->
+      <p class="text-xl"><?= $output ?></p>
+      <p>
+      <pre>
+          <?php print_r($listings) ?>
+        </pre>
+      </p>
+    </div>
+  </div>
+</body>
+
+</html>
