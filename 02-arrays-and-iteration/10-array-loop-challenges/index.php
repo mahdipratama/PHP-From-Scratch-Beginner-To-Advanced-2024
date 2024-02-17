b07656dfcd12cbcd90cb3fa041a99b2e1b10e56d
mahdipratama <mahdipratama29@gmail.com>
2024-02-18 00:32:45 +0700

    add Array Loop Challenges

--- 02-arrays-and-iteration/10-array-loop-challenges/index.php
+++ 02-arrays-and-iteration/10-array-loop-challenges/index.php
@@ -22,6 +22,12 @@
 */
 echo '<h3>Multiplication Table</h3>';
 
+for ($i = 1; $i <= 10; $i++) {
+  for ($j = 1; $j <= 10; $j++) {
+    echo $i . ' x ' . $j . ' = ' . $i * $j . '<br />';
+  }
+};
+
 /*
   Challenge 2: Get the sum of the numbers in an array by using a foreach loop. For bonus points, also use a for loop.
 */
@@ -30,14 +36,63 @@ echo '<h3>Array Sum</h3>';
 
 $numbers = [1, 2, 3, 4, 5];
 
+// Foreach loop
+$sum = 0;
+foreach ($numbers as $number) {
+  $sum += $number;
+};
+
+print_r($sum);
+
+echo '<br/>';
+echo '<br/>';
+
+// For... loop
+$sum2 = 0;
+for ($i = 0; $i <= count($numbers); $i++) {
+  $sum2 += $i;
+}
+print_r($sum2);
+
 /*
   Challenge 3: Calculate the average students grade from an array of students. Each student has their own array with the key 'grades'. 
 
-  	1. Create an array of students with their names and grades (0 - 100)
+  1. Create an array of students with their names and grades (0 - 100)
 	2. Iterate over the students array with a foreach loop
 	3. Calculate the average grade for each student
 */
 
 echo '<h3>Average Grade</h3>';
 
+$students = [
+  [
+    'name' => 'Emma',
+    'grades' => [89, 99, 79, 98, 85]
+  ],
+  [
+    'name' => 'Wilson',
+    'grades' => [70, 77, 79, 65, 50]
+  ],
+  [
+    'name' => 'Watson',
+    'grades' => [80, 88, 75, 70, 85]
+  ],
+];
 
+
+
+foreach ($students as $student) {
+  echo 'name: ' . $student['name'] . '<br/>';
+  echo 'grades: ' . implode(', ', $student['grades']) . '<br/>';
+
+  // My Solution 
+  // $average = 0;
+  // foreach ($student['grades'] as $number) {
+  //   $average += $number / count($student['grades']);
+  // };
+
+  // Brad Solution
+  $average = array_sum($student['grades']) / count($student['grades']);
+
+  echo 'averages: ' . $average . '<br/>' . '<br/>';
+}
