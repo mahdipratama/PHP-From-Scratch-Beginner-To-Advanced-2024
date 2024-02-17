50028f792273fb800e37cf4c08b871d60a4f14b7
mahdipratama <mahdipratama29@gmail.com>
2024-02-17 18:56:17 +0700

    add Associative Arrays lecture

--- 02-arrays-and-iteration/03-associative-arrays/index.php
+++ 02-arrays-and-iteration/03-associative-arrays/index.php
@@ -1,5 +1,71 @@
 <?php
 $output = null;
+
+$user = [
+    'name' => 'John',
+    'email' => 'John@gmail.com',
+    'password' => '123456',
+    'hobbies' => ['tennis', 'video games',]
+];
+
+/*  output : 
+User Array:
+     Array
+(
+    [name] => John
+    [email] => John@gmail.com
+    [password] => 123456
+    [hobbies] => Array
+        (
+            [0] => tennis
+            [1] => video games
+        )
+
+)
+*/
+
+// access elements
+$output = $user['name']; // John
+$output = $user['email']; // John@gmail.com
+
+$output = $user['hobbies'][0]; // tennis
+
+// add element
+$user['address'] = '12 main St';
+/*  output : 
+User Array:
+     Array
+(
+    [name] => John
+    [email] => John@gmail.com
+    [password] => 123456
+    [hobbies] => Array
+        (
+            [0] => tennis
+            [1] => video games
+        )
+    [address] => 12 main St
+)
+*/
+
+// Delete element
+unset($user['address']);
+/*  output : 
+User Array:
+     Array
+(
+    [name] => John
+    [email] => John@gmail.com
+    [password] => 123456
+    [hobbies] => Array
+        (
+            [0] => tennis
+            [1] => video games
+        )
+
+)
+*/
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +88,14 @@ $output = null;
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">
+                User Array:
+            </h2>
+            <p>
+            <pre>
+                    <?php print_r($user) ?>
+                </pre>
+            </p>
         </div>
     </div>
 </body>
