4ad9ae10e8d41935654b99de6a6f7ee42a74e05b
mahdipratama <mahdipratama29@gmail.com>
2024-02-18 19:20:16 +0700

    add Variable Scope lecture

---
+++ 04-functions/03-variable-scope/index.php
@@ -0,0 +1,29 @@
+<?php
+
+// Global Scope
+$name = 'Alice';
+
+function sayHello()
+{
+  // Local Scope
+  global $name; // We can use the global keyword to access a global variable inside a function
+  $name = 'Bob'; // Changing the value will only affect the local scope
+  echo 'Hello ' . $name;
+}
+
+echo $name; // Alice
+
+sayHello(); // Bob
+
+echo $name; // Bob
+
+
+function sayGoodbye()
+{
+  $names = ['John', 'Brad', 'Alice'];
+  echo 'Goodbye ' . $names[1];
+}
+
+sayGoodbye();
+
+echo $names[1]; // This will throw an error because $names is not defined in the global scope
