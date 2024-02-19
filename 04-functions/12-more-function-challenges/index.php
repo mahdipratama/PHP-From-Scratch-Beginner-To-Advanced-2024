5090c17ead11050bc06b8ae97d205da064b37605
mahdipratama <mahdipratama29@gmail.com>
2024-02-19 17:35:16 +0700

    add More Functions Challenges

--- 04-functions/12-more-function-challenges/index.php
+++ 04-functions/12-more-function-challenges/index.php
@@ -6,12 +6,50 @@
   The formula to convert Fahrenheit to Celsius is: Celsius = (Fahrenheit - 32) * 5/9
 */
 
+// Solution
+function fahrenheitToCelsius($fahrenheit)
+{
+  $celcius = ($fahrenheit - 32) * 5 / 9;
+
+  return "{$fahrenheit}F° = {$celcius}C°";
+};
+
+echo fahrenheitToCelsius(68); // 68F° = 20C°
+
+$baseTemp =  32;
+$fahrenheitToCelsius2 = function ($f) use ($baseTemp) {
+  $celcius = ($f - $baseTemp) * 5 / 9;
+
+  return "{$f}F° = {$celcius}C°";
+};
+
+echo $fahrenheitToCelsius2(68); // 68F° = 20C°
+
+
 echo '<br>';
 
 /*
   Challenge 2: Print names in uppercase
   Create a function called `printNamesToUpperCase` that takes an array of names as an argument. The function should loop through the array and print each name to the screen in uppercase letters.
 */
+$names = ['Nami', 'Wilson', 'John', 'Anderson'];
+
+function printNamesToUpperCase($names)
+{
+  foreach ($names as $name) {
+    echo strtoupper($name) . ', ';
+  }
+};
+
+printNamesToUpperCase($names); // NAMI, WILSON, JOHN, ANDERSON,
+
+
+// Solution 2 
+$printNames = fn ($name) => strtoupper($name);
+
+$printNamesToUpperCase2 = array_map($printNames, $names);
+
+print_r(implode(', ', $printNamesToUpperCase2)); // NAMI, WILSON, JOHN, ANDERSON
 
 
 echo '<br>';
@@ -22,3 +60,28 @@ echo '<br>';
   2. The function should return the longest word in the sentence.
   3. The output should look like this:
 */
+
+$sentence = 'The quick brown fox jumped over the lazy dog on facebook';
+
+function findLongestWord($sentence)
+{
+  $words = explode(' ', $sentence);
+
+  $longestWord = '';
+
+  foreach ($words as $word) {
+    $word = trim($word);
+
+    // Check if the current word is longer than the Longest word
+    if (strlen($word) > strlen($longestWord) || $longestWord === '') {
+      $longestWord = $word;
+    }
+  }
+
+  return $longestWord;
+};
+
+$longestWord =
+  findLongestWord($sentence);
+
+echo $longestWord; // facebook
