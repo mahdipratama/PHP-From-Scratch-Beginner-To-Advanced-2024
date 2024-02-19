b42e4f2da2f3a87965d5cf763a559b8538552838
mahdipratama <mahdipratama29@gmail.com>
2024-02-19 20:44:46 +0700

    add Access Modifier Getters and Setters lecture

--- 05-oop/02-access-modifiers-getters-setters/index.php
+++ 05-oop/02-access-modifiers-getters-setters/index.php
@@ -1,10 +1,23 @@
 <?php
 
+/*
+  - public
+  The property or method can be accessed from anywhere. This is the default if you leave off the modifier
+
+  - protected
+  The property or method can be accessed from within the class OR any class that inherits from it
+
+  - private
+  The property can ONLY be accessed from within the class
+*/
+
 class User
 {
   // Properties
   public $name;
   public $email;
+  protected $status = 'active'; // still can accessed from Getter and Setter
+  // private $status = 'active';
 
   public function __construct($name, $email)
   {
@@ -17,6 +30,18 @@ class User
   {
     echo $this->name . ' logged in <br>';
   }
+
+  // Getters , to access private properties
+  public function getStatus()
+  {
+    echo $this->status;
+  }
+
+  // Setters, to modify private properties
+  public function setStatus($status)
+  {
+    $this->status = $status;
+  }
 }
 
 // Instantiate a new object
@@ -27,5 +52,15 @@ $user1->login();
 $user2 = new User('Jane Doe', 'jane@gmail.com');
 
 $user2->login();
+
+// set status 
+$user2->setStatus('inactive'); // inactive
+
+// get status
+$user2->getStatus(); // active
+
+// try to access protected property
+// $user2->status; // Fatal error: Uncaught Error: Cannot access private property User
+
 
 // var_dump($user2);
