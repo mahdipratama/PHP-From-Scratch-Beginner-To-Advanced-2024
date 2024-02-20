2c1912bb442b80fd622b1e0610ccd5c1d21a4d70
mahdipratama <mahdipratama29@gmail.com>
2024-02-20 15:31:40 +0700

    add Get query from URL String lecture

--- 06-superglobals/03-get/index.php
+++ 06-superglobals/03-get/index.php
@@ -1,3 +1,9 @@
 <?php
 // Add a query parameter to the URL in the browser's address bar:
 // http://localhost:8000/?name=John
+
+// Getting variables / values from the URL(query string)
+var_dump($_GET); // array(1) { ["name"]=> string(4) "john" }
+
+echo isset($_GET['name']) ? $_GET['name'] : ''; // john
+echo $_GET['name'] ?? ''; // john
