7454366247b79963fe43c5f84a19e1a7f6b32f97
mahdipratama <mahdipratama29@gmail.com>
2024-02-20 12:19:37 +0700

    add Interfaces lecture

--- 05-oop/07-interfaces/index.php
+++ 05-oop/07-interfaces/index.php
@@ -1,3 +1,64 @@
+<?php
+// Interfaces it's a collection of abstract method and constant. class that implement Interfaces must implement all of the method and constant in that interface. Interface can't be instantiate 
+
+interface ContentInterface
+{
+  public function display();
+  public function edit();
+}
+
+class Article implements ContentInterface
+{
+  private $title;
+  private $content;
+
+  public function __construct($title, $content)
+  {
+    $this->title = $title;
+    $this->content = $content;
+  }
+
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<p>{$this->content}</p>";
+  }
+
+  public function edit()
+  {
+    echo "Editing the article '{$this->title}' ";
+  }
+}
+
+class Video implements ContentInterface
+{
+  private $title;
+  private $url;
+
+  public function __construct($title, $url)
+  {
+    $this->title = $title;
+    $this->url = $url;
+  }
+
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<iframe src= '{$this->url}'></iframe>";
+  }
+
+  public function edit()
+  {
+    echo "Editing the video '{$this->title}' ";
+  }
+}
+
+$article = new Article('Introduction to PHP', 'PHP is a versatile scripting language');
+
+$video = new Video('PHP for Beginners', 'https://www.youtube.com/watch?v=BUCiSSyIGGU')
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,9 +76,17 @@
     </div>
   </header>
   <div class="container mx-auto p-4 mt-4">
+    <!-- Output -->
+
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
+      <?= $article->display() ?>
     </div>
+
+    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <?= $video->display() ?>
+    </div>
+
+
   </div>
 </body>
 
