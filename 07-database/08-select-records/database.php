00ea68ad0d3bcea312db7eca4aed11cf4e95309f
mahdipratama <mahdipratama29@gmail.com>
2024-02-21 20:41:42 +0700

    Fetch Multiple Records lecture

--- 07-database/08-select-records/database.php
+++ 07-database/08-select-records/database.php
@@ -5,7 +5,7 @@ $host = 'localhost';
 $port = 3306;
 $dbName = 'blog';
 $username = 'root';
-$password = '';
+$password = 'dontcry09';
 
 // Connection string (DSN)
 $dsn = "mysql:host={$host};port={$port};dbname={$dbName};charset=utf8";
@@ -16,6 +16,9 @@ try {
 
   // Set PDO to throw exceptions on error
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // Fetch as Assoc array
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
   // If there is an error with the connection, catch it here
   echo "Connection failed: " . $e->getMessage();
